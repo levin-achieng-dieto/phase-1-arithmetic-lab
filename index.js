c1293892e2de2acddcea0c59779af9cfada2d17f
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-18 01:20:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 *num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 4
+let num4 = 7
+const mod = num3 % num4;
+
+const max = Math.max(2, 4, 1, 8, 3,20);
